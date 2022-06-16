4fa4e2d31198936cee514705b10c1113e33f6ab1
elramirez <42379702+elramirezn@users.noreply.github.com>
2022-06-16 06:59:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const conectarDB = require("./src/database");
 const cors = require("cors");
 const app = express();
 
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
 conectarDB();
 app.use(express.json());
 app.use(cors());
 app.use("/api/crud", require("./src/routes/crud"));
 
-app.listen(4000, () => {
-    console.log(`Listening http://localhost:${4000}`);
+app.listen(PORT, () => {
+    console.log(`Listening http://localhost:${PORT}`);
 });
